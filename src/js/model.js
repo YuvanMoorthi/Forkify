f45090d9b696361a36288d4d4436dd01563017d7
YuvanMoorthi <yuvanmoorthi34@gmail.com>
2025-06-23 00:23:04 +0530

    6

--- src/js/model.js
+++ src/js/model.js
@@ -36,8 +36,6 @@ export const loadRecipe = async function (id) {
     if (state.bookmarks.some(bookmark => bookmark.id === id))
       state.recipe.bookmarked = true;
     else state.recipe.bookmarked = false;
-
-    console.log(state.recipe);
   } catch (err) {
     // Temp error handling
     console.error(`${err} 💥💥💥💥`);
@@ -50,7 +48,7 @@ export const loadSearchResults = async function (query) {
     state.search.query = query;
 
     const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
-    console.log(data);
+
 
     state.search.results = data.data.recipes.map(rec => {
       return {
