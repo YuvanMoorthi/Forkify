7db52380bcfc581b591586fc346e7a56206c2234
YuvanMoorthi <yuvanmoorthi34@gmail.com>
2025-06-22 20:04:06 +0530

    Initial commit

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = '5ac2e643-8096-485e-97b2-c9630cca9c8a';
 export const MODAL_CLOSE_SEC = 2.5;
