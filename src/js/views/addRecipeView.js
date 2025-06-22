5eba5a26815d7797131638f0c4a7786f8ffeeb12
YuvanMoorthi <yuvanmoorthi34@gmail.com>
2025-06-22 21:09:07 +0530

    Fix: Correct Parcel 2 asset imports for icons

--- src/js/views/addRecipeView.js
+++ src/js/views/addRecipeView.js
@@ -1,5 +1,5 @@
 import View from './View.js';
-import icons from 'url:../../img/icons.svg'; // Parcel 2
+import icons from '../../img/icons.svg';
 
 class AddRecipeView extends View {
   _parentElement = document.querySelector('.upload');
@@ -39,7 +39,7 @@ class AddRecipeView extends View {
     });
   }
 
-  _generateMarkup() {}
+  _generateMarkup() { }
 }
 
 export default new AddRecipeView();
